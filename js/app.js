e7db297fe1c57047cb90efe6e9825098528b8fe5
Mike Allison <mikeallison79@gmail.com>
2016-08-24 16:50:49 -0400

    Finished v1.0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  $('.hero-text').css('visibility', 'visible').hide().fadeIn(2000);
+
+  $('.menu-small').click(function() {
+    $('.menu-small-list').toggleClass('open');
+    $('.menu-small-list ul').fadeToggle('fast');
+  });
+
+  $('.menu-small-list a').click(function() {
+    $('.menu-small-list').removeClass('open');
+    $('.menu-small-list ul').removeAttr('style');
+  });
+});
